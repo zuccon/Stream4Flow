d2aa967892eefc6cf7f2f9556e21e72e3b93518c
severinsimko@gmail.com <severinsimko@gmail.com>
2017-03-07 18:27:48 +0100

    Global functions created

--- web-interface/Stream4Flow/controllers/system.py
+++ web-interface/Stream4Flow/controllers/system.py
@@ -4,14 +4,15 @@ from __future__ import division
 import hashlib
 # Enable random string generator
 import random
+import time
 
 import json, requests, urllib
 # Enable to get current datetime
 import os, subprocess
 from datetime import datetime
 # Import global functions
-from global_functions import escape
-from global_functions import check_username
+from global_functions import escape,check_username
+
 
 
 # ----------------- Common Settings ------------------#
@@ -276,9 +277,12 @@ def about():
     """
     return dict()
 
+# ----------------- Applications ----------------------------#
 
-def applications():
-    # Get duration and app information
+#Get all applications
+
+def get_applications():
+
     my_dict = {}
     final_dict = {}
     try:
@@ -287,17 +291,18 @@ def applications():
         data = r.json()
 
         applications_idct = dict(
-         [(item["id"], (item["state"], item["name"], item["duration"], item["cores"], item["memoryperslave"])) for item
-          in data["activeapps"]])
+            [(item["id"], (item["state"], item["name"], item["duration"], item["cores"], item["memoryperslave"])) for
+             item
+             in data["activeapps"]])
     except (requests.ConnectionError):
         return dict(
             alert_type2="danger",
             alert_message2="The cluster is not running!",
             applications=final_dict)
 
-    for x in range(1, 5):
+    for x in range(0, 2):
         try:
-            s = requests.get('http://10.16.31.211:404' + str(x) + '/metrics/json/')
+            s = requests.get('http://10.16.31.211:405' + str(x) + '/metrics/json/')
             s.encoding = 'UTF-8'
             records = s.json()
             for key, values in records.iteritems():
@@ -310,7 +315,6 @@ def applications():
         except (ValueError, IOError):
             continue
 
-
     for id, (state, name, duration, cores, memory) in applications_idct.items():
         if state == "RUNNING":
             for appid, records in my_dict.items():
@@ -319,94 +323,13 @@ def applications():
                     averageRPS = records / (duration / 1000)
                     timeInHours = str(round(runningTime, 2))
                     final_dict[id] = [name, timeInHours, records, averageRPS, cores, memory]
+    return final_dict
 
-    return dict(applications=final_dict)
-
-
-def cluster():
-    workers_idct = {}
-    try:
-        urlWorkers = 'http://10.16.31.211:8080/json/'
-        responseWorkers = urllib.urlopen(urlWorkers)
-        dataWorkers = json.loads(responseWorkers.read())
-        workers_idct = {}
-        for item in dataWorkers["workers"]:
-            if item["state"] == "ALIVE":
-                workers_idct[item["id"]] = [item["webuiaddress"], item["cores"], item["memory"], item["state"]]
-    except(ValueError, IOError):
-        print "Cluster nebezi"
-
-    return dict(workers=workers_idct)
-
-
-def clusterStart():
-
-    url = "http://10.16.31.211:3031/cluster/start"
-    responseCluster = urllib.urlopen(url)
-    dataCluster = responseCluster.read()
-
-    if "Cluster was already running!" == dataCluster:
-        alert_type2="danger"
-
-    if "Cluster sucessfully started!" == dataCluster:
-        alert_type2 = "success"
-
-    workers_idct = {}
-    try:
-        urlWorkers = 'http://10.16.31.211:8080/json/'
-        responseWorkers = urllib.urlopen(urlWorkers)
-        dataWorkers = json.loads(responseWorkers.read())
-        workers_idct = {}
-        for item in dataWorkers["workers"]:
-            if item["state"] == "ALIVE":
-                workers_idct[item["id"]] = [item["webuiaddress"], item["cores"], item["memory"], item["state"]]
-    except(ValueError, IOError):
-        print "Cluster nebezi"
-
-    response.view = request.controller + '/cluster.html'
-    return dict(
-        alert_type2=alert_type2,
-        alert_message2=dataCluster,
-        workers=workers_idct
-    )
-
-
-def clusterKill():
-    url = "http://10.16.31.211:3031/cluster/kill"
-    responseCluster = urllib.urlopen(url)
-    dataCluster = responseCluster.read()
-
-    if "Cluster was already not running!" == dataCluster:
-        alert_type2="danger"
-
-    if "Cluster sucessfully killed!" == dataCluster:
-        alert_type2 = "success"
-
-    workers_idct = {}
-    try:
-        urlWorkers = 'http://10.16.31.211:8080/json/'
-        responseWorkers = urllib.urlopen(urlWorkers)
-        dataWorkers = json.loads(responseWorkers.read())
-        workers_idct = {}
-        for item in dataWorkers["workers"]:
-            if item["state"] == "ALIVE":
-                workers_idct[item["id"]] = [item["webuiaddress"], item["cores"], item["memory"], item["state"]]
-    except(ValueError, IOError):
-        print "Cluster nebezi"
-
-    response.view = request.controller + '/cluster.html'
-    return dict(
-        alert_type2=alert_type2,
-        alert_message2=dataCluster,
-        workers=workers_idct
-    )
-
-
-
+def applications():
+    return dict(applications=get_applications())
 
 def startApplication():
-    my_dict = {}
-    final_dict = {}
+
     selectApplication = request.post_vars.get('application')
     selectMemory = request.post_vars.get('cores')
     selectCores = request.post_vars.get('cores')
@@ -421,56 +344,17 @@ def startApplication():
         url = "http://10.16.31.211:3031/" +selectApplication + "/cores=" +str(selectCores) + "/memory=" + str(selectMemory)
         data = requests.get(url).json
 
-    try:
-        r = requests.get('http://10.16.31.211:8080/json/')
-        r.encoding = 'UTF-8'
-        data = r.json()
-
-        applications_idct = dict(
-            [(item["id"], (item["state"], item["name"], item["duration"], item["cores"], item["memoryperslave"])) for item in data["activeapps"]])
-
-    except (requests.ConnectionError):
-        response.view = request.controller + '/applications.html'
-        return dict(
-            alert_type2="danger",
-            alert_message2="You cannot run application because the cluster is not running!",
-            applications=final_dict)
-
-
-
-    for x in range(1, 5):
-        try:
-            s = requests.get('http://10.16.31.211:404' + str(x) + '/metrics/json/')
-            s.encoding = 'UTF-8'
-            records = s.json()
-            for key, values in records.iteritems():
-                if key == "gauges":
-                    for keygauge, valgauge in values.iteritems():
-                        if "StreamingMetrics.streaming.totalReceivedRecords" in keygauge:
-                            my_dict[keygauge] = valgauge.values()[0]
-                            break
-
-        except (ValueError, IOError):
-            continue
-
-
-    for id, (state, name, duration,cores,memory) in applications_idct.items():
-        if state == "RUNNING":
-            for appid, records in my_dict.items():
-                if id in appid:
-                    runningTime = duration / 3600000
-                    averageRPS = records / (duration / 1000)
-                    timeInHours = str(round(runningTime, 2))
-                    final_dict[id] = [name, timeInHours, records, averageRPS,cores,memory]
+    running_apps_dict = get_applications()
 
     response.view = request.controller + '/applications.html'
     return dict(
             alert_type2=alert_type2,
             alert_message2=alert_message,
-            applications=final_dict
+            applications=running_apps_dict
         )
 
 def killApplication():
+
     selectApplication = request.post_vars.get('appname')
     if "protocols_statistics" in selectApplication:
 
@@ -485,44 +369,80 @@ def killApplication():
 
     alert_message = "Application " + selectApplication + " killed!"
 
-
-    r = requests.get('http://10.16.31.211:8080/json/')
-    r.encoding = 'UTF-8'
-    data = r.json()
-
-    applications_idct = dict(
-        [(item["id"], (item["state"], item["name"], item["duration"], item["cores"], item["memoryperslave"])) for item
-         in data["activeapps"]])
-
-    my_dict = {}
-    for x in range(1, 5):
-        try:
-            s = requests.get('http://10.16.31.211:404' + str(x) + '/metrics/json/')
-            s.encoding = 'UTF-8'
-            records = s.json()
-            for key, values in records.iteritems():
-                if key == "gauges":
-                    for keygauge, valgauge in values.iteritems():
-                        if "StreamingMetrics.streaming.totalReceivedRecords" in keygauge:
-                            my_dict[keygauge] = valgauge.values()[0]
-                            break
-
-        except (ValueError, IOError):
-            continue
-
-    final_dict = {}
-    for id, (state, name, duration, cores, memory) in applications_idct.items():
-        if state == "RUNNING":
-            for appid, records in my_dict.items():
-                if id in appid:
-                    runningTime = duration / 3600000
-                    averageRPS = records / (duration / 1000)
-                    timeInHours = str(round(runningTime, 2))
-                    final_dict[id] = [name, timeInHours, records, averageRPS, cores, memory]
+    running_apps_dict = get_applications()
 
     response.view = request.controller + '/applications.html'
     return dict(
         alert_type2=alert_type2,
-        alert_message2=selectApplication,
-        applications=final_dict
+        alert_message2=alert_message,
+        applications=running_apps_dict
     )
+
+# ----------------- Cluster ----------------------------#
+
+
+
+
+def cluster_info():
+    workers_idct = {}
+    try:
+        urlWorkers = 'http://10.16.31.211:8080/json/'
+        responseWorkers = urllib.urlopen(urlWorkers)
+        dataWorkers = json.loads(responseWorkers.read())
+        workers_idct = {}
+        for item in dataWorkers["workers"]:
+            if item["state"] == "ALIVE":
+                workers_idct[item["id"]] = [item["webuiaddress"], item["cores"], item["memory"], item["state"]]
+    except(ValueError, IOError):
+        print "Cluster nebezi"
+
+    return workers_idct
+
+def cluster():
+    return dict(workers=cluster_info())
+
+def clusterStart():
+
+    url = "http://10.16.31.211:3031/cluster/start"
+    responseCluster = urllib.urlopen(url)
+    dataCluster = responseCluster.read()
+
+    if "Cluster was already running!" == dataCluster:
+        alert_type2="danger"
+
+    if "Cluster sucessfully started!" == dataCluster:
+        alert_type2 = "success"
+
+    active_workers = cluster_info()
+
+    response.view = request.controller + '/cluster.html'
+    return dict(
+        alert_type2=alert_type2,
+        alert_message2=dataCluster,
+        workers=active_workers
+    )
+
+
+def clusterKill():
+    url = "http://10.16.31.211:3031/cluster/kill"
+    responseCluster = urllib.urlopen(url)
+    dataCluster = responseCluster.read()
+
+    if "Cluster was already not running!" == dataCluster:
+        alert_type2="danger"
+
+    if "Cluster sucessfully killed!" == dataCluster:
+        alert_type2 = "success"
+
+    active_workers = cluster_info()
+
+    response.view = request.controller + '/cluster.html'
+    return dict(
+        alert_type2=alert_type2,
+        alert_message2=dataCluster,
+        workers=active_workers
+    )
+
+
+
+
--- web-interface/Stream4Flow/modules/global_functions.py
+++ web-interface/Stream4Flow/modules/global_functions.py
@@ -54,4 +54,3 @@ def check_password(db, username, password):
     if db(db.users_auth.user_id == user_id and db.users_auth.password == hash).select():
         return True
     return False
-
